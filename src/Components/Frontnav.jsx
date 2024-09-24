f2ee2de2eb32b0e601298a98c1b4980b47fce42f
Sumathikirubhakar <kirubhakarme14101994@gmail.com>
2024-09-24 17:14:29 +0530

    design update

--- src/Components/Frontnav.jsx
+++ src/Components/Frontnav.jsx
@@ -3,13 +3,13 @@ import './Frontnav.css'
 
 export default function Frontnav() {
     return (
-        <div>
+        <div className='main-nav'>
             <div class="a">
                 <p class="welcome">Welcome to SK Foods and Snacks...! <span><br />Feel and Smell Good.</span></p>
             </div>
             <div class="b">
                 <nav class="lists">
-                    <img src="sk logo.png" className='rounded-circle' alt="no image" height={100} width={100} />
+                    <img src="sk logo.png" className='rounded-circle' alt="no image" height={50} width={50} />
                     <ul class="menu">
                         <li class="items"><a href="home">HOME</a></li>
                         <li class="items"><a href="profile">COMPANY PROFILE</a></li>
