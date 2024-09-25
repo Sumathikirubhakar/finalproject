52e5f9c145c030ef34d5223990da06ce34309b15
Sumathikirubhakar <kirubhakarme14101994@gmail.com>
2024-09-25 17:04:56 +0530

    experience update

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -9,46 +9,48 @@ export default function Experience() {
         <p class="theory">SK Foods and Snacks can tasting helps people understand the sensory qualities of food and beverages, which can help improve food quality and consumer satisfaction.</p>
       </div>
       <div class="eatingplace">
-        <img src="eateatinghall.png" alt="no image" height={500} width={1550}/>
+        <img src="eateatinghall.png" alt="no image" height={500} width={1550} />
       </div>
 
-      <div class="tables">
-        <table>
-      <thead>
-        <tr>
-          <th>PRODUCTS</th>
-          <th>QUICK LINKS</th>
-          <th>LEGAL</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Sweets</td>
-          <td>Home</td>
-          <td>Terms & Conditions</td>
-        </tr>
-        <tr>
-          <td>Restaurant</td>
-          <td>Brand Story</td>
-          <td>Cookie Ploicy</td>
-        </tr>
-        <tr>
-          <td>Retail</td>
-          <td>Center Of Excellence</td>
-          <td>Trademark & Copyright Policy</td>
-        </tr>
-        <tr>
-          <td></td>
-          <td>Media</td>
-          <td>Privacy Policy</td>
-        </tr>
-        <tr>
-          <td></td>
-          <td>Contact US</td>
-          <td></td>
-        </tr>
-      </tbody>
-        </table>
+      <div className="row">
+        <div className="col-md-3">
+          <h5>PRODUCTS</h5>
+          <p>Sweets</p>
+          <p>Restaurant</p>
+          <p>Retail</p>
+
+        </div>
+        <div className="col-md-3">
+          <h5>QUICK LINKS</h5>
+          <p>Home</p>
+          <p>Brand Story</p>
+          <p>Center Of Excellence</p>
+          <p>Media</p>
+          <p>Contact US</p>
+
+        </div>
+        <div className="col-md-3">
+          <h5>LEGAL</h5>
+          <p>Terms & Conditions</p>
+          <p>Cookie Ploicy</p>
+          <p>Trademark & Copyright Policy</p>
+          <p>Privacy Policy</p>
+        </div>
+        <div className="col-md-3">
+          <h5>Address</h5>
+          <p>No: 53, Ambattur Industrial Estate, 3rd st, <br />
+            South phase, Ambattur, Chennai - 600058</p>
+
+          <h5>Phone</h5>
+          <p>+91-44-42333333 / +91-44-40999999</p>
+
+          <h5>Email</h5>
+          <p>care@aabsweets.com</p>
+
+          <h5>Timing</h5>
+          <p>Monday To Sunday <br />
+            7:00 AM to 10:00 PM</p>
+        </div>
       </div>
     </div>
   )
